8445d982792d8d7cd841256455c42937c24c49fc
HBB20 <harshbbhakta@gmail.com>
2016-01-20 15:55:52 +0530

    Init commit

---
+++ app/src/main/java/in/hbb20/countrycodepickerproject/ExampleActivity.java
@@ -0,0 +1,13 @@
+package in.hbb20.countrycodepickerproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
