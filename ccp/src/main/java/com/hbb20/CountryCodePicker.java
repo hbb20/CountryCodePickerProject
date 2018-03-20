968ea2cfd3b065ebddecd26b0549718692b1821b
HBB20 <harshbbhakta@gmail.com>
2018-03-20 12:43:30 -0500

    - Option to add hint number format

--- ccp/src/main/java/com/hbb20/CountryCodePicker.java
+++ ccp/src/main/java/com/hbb20/CountryCodePicker.java
@@ -79,6 +79,7 @@ public class CountryCodePicker extends RelativeLayout {
     boolean detectCountryWithAreaCode = true;
     boolean ccpDialogShowNameCode = true;
     PhoneNumberType hintExampleNumberType = PhoneNumberType.MOBILE;
+    HintNumberFormat hintExampleNumberFormat = HintNumberFormat.DEFAULT;
     String selectionMemoryTag = "ccp_last_selection";
     int contentColor;
     int borderFlagColor;
@@ -229,6 +230,10 @@ public class CountryCodePicker extends RelativeLayout {
             //example number hint type
             int hintNumberTypeIndex = a.getInt(R.styleable.CountryCodePicker_ccp_hintExampleNumberType, 0);
             hintExampleNumberType = PhoneNumberType.values()[hintNumberTypeIndex];
+
+            //example number hint format
+            int hintNumberFormatIndex = a.getInt(R.styleable.CountryCodePicker_ccp_hintExampleNumberFormat, 0);
+            hintExampleNumberFormat = HintNumberFormat.values()[hintNumberFormatIndex];
 
             //memory tag name for selection
             selectionMemoryTag = a.getString(R.styleable.CountryCodePicker_ccp_selectionMemoryTag);
@@ -464,18 +469,18 @@ public class CountryCodePicker extends RelativeLayout {
 
     /**
      * To show/hide name code from country selection dialog
+     */
+    public boolean getCcpDialogShowNameCode() {
+        return this.ccpDialogShowNameCode;
+    }
+
+    /**
+     * To show/hide name code from country selection dialog
      *
      * @param ccpDialogShowNameCode
      */
     public void setCcpDialogShowNameCode(boolean ccpDialogShowNameCode) {
         this.ccpDialogShowNameCode = ccpDialogShowNameCode;
-    }
-
-    /**
-     * To show/hide name code from country selection dialog
-     */
-    public boolean getCcpDialogShowNameCode() {
-        return this.ccpDialogShowNameCode;
     }
 
     boolean isShowPhoneCode() {
@@ -723,20 +728,25 @@ public class CountryCodePicker extends RelativeLayout {
      */
     private void updateHint() {
         if (editText_registeredCarrierNumber != null && hintExampleNumberEnabled) {
+            String formattedNumber = "";
             Phonenumber.PhoneNumber exampleNumber = getPhoneUtil().getExampleNumberForType(getSelectedCountryNameCode(), getSelectedHintNumberType());
             if (exampleNumber != null) {
-                String formattedNumber = exampleNumber.getNationalNumber() + "";
-                Log.d(TAG, "updateHint: " + formattedNumber);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    formattedNumber = PhoneNumberUtils.formatNumber(formattedNumber, getSelectedCountryNameCode());
-                } else {
-                    formattedNumber = PhoneNumberUtils.formatNumber(formattedNumber);
+                if (hintExampleNumberFormat == HintNumberFormat.DEFAULT) {
+                    formattedNumber = exampleNumber.getNationalNumber() + "";
+                    Log.d(TAG, "updateHint: " + formattedNumber);
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        formattedNumber = PhoneNumberUtils.formatNumber(formattedNumber, getSelectedCountryNameCode());
+                    } else {
+                        formattedNumber = PhoneNumberUtils.formatNumber(formattedNumber);
+                    }
+                } else if (hintExampleNumberFormat == HintNumberFormat.NATIONAL) {
+                    formattedNumber = getPhoneUtil().format(exampleNumber, PhoneNumberUtil.PhoneNumberFormat.NATIONAL);
                 }
-                editText_registeredCarrierNumber.setHint(formattedNumber);
                 Log.d(TAG, "updateHint: after format " + formattedNumber + " " + selectionMemoryTag);
             } else {
                 Log.w(TAG, "updateHint: No example number found for this country (" + getSelectedCountryNameCode() + ") or this type (" + hintExampleNumberType.name() + ").");
             }
+            editText_registeredCarrierNumber.setHint(formattedNumber);
         }
     }
 
@@ -2057,6 +2067,15 @@ public class CountryCodePicker extends RelativeLayout {
         updateHint();
     }
 
+    public HintNumberFormat getHintExampleNumberFormat() {
+        return hintExampleNumberFormat;
+    }
+
+    public void setHintExampleNumberFormat(HintNumberFormat hintExampleNumberFormat) {
+        this.hintExampleNumberFormat = hintExampleNumberFormat;
+        updateHint();
+    }
+
     /**
      * Update every time new language is supported #languageSupport
      */
@@ -2100,6 +2119,8 @@ public class CountryCodePicker extends RelativeLayout {
             this.code = code;
         }
     }
+
+    public enum HintNumberFormat {DEFAULT, NATIONAL}
 
     public enum PhoneNumberType {
         MOBILE,
